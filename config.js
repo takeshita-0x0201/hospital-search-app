a9c7cd03e20d02d40a1ef81f2d65db5e54158766
takeshita-0x0201 <takeshita.0x0201@gmail.com>
2025-03-16 12:58:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-const API_KEY = "AIzaSyBMpuPyp44-V8JgrHyLgeO-tFeQBoUWvfQ";
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzGsea9yGQf7tlD5LXl0j2oRaZdPAEff1h6j8m09iM-J-EJtFJQ3P4MDhPOrMwFIslU-w/exec";
+
