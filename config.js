0470860a76e931f05716353b817c81f4535979c5
takeshita-0x0201 <takeshita.0x0201@gmail.com>
2025-03-16 12:30:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_KEY = "AIzaSyBMpuPyp44-V8JgrHyLgeO-tFeQBoUWvfQ";
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyiLKPo5MwrRNjV6l7klb1CIJ8UUcFM4vDioXWQ4MJD-Ss1Sv3xwGS_LmHw5WeE10WI/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzGsea9yGQf7tlD5LXl0j2oRaZdPAEff1h6j8m09iM-J-EJtFJQ3P4MDhPOrMwFIslU-w/exec";
