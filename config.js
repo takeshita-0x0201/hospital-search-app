dbf959f7e866c6357eb13c4d6d9c0592ca3227a0
takeshita-0x0201 <takeshita.0x0201@gmail.com>
2025-03-16 12:06:19 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const API_KEY = "AIzaSyBMpuPyp44-V8JgrHyLgeO-tFeQBoUWvfQ";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyiLKPo5MwrRNjV6l7klb1CIJ8UUcFM4vDioXWQ4MJD-Ss1Sv3xwGS_LmHw5WeE10WI/exec";
